f815234dcd10ceeb9a02e68b3b200f1e07d8a224
DDDDDavid <david7749@163.com>
2021-04-23 00:39:31 +0800

    question 5

--- leetcode/leetcode_202104.py
+++ leetcode/leetcode_202104.py
@@ -147,6 +147,24 @@ class Solution:
         else:
             return (getKthElement(totalLength // 2) + getKthElement(totalLength // 2 + 1)) / 2
 
+## 5.给你一个字符串 s，找到 s 中最长的回文子串。
+#中心扩展法
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            left2, right2 = self.expandAroundCenter(s, i, i + 1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
         
         
 
